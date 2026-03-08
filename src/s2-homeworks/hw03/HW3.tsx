4909a9f1f854f13401a45d812b993925b9cb1ed9
andrew_guk <gukandrey91@gmail.com>
2026-03-08 18:12:23 +0300

    HW3 - add

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {Dispatch, SetStateAction, useState} from 'react'
 import { v1 } from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
@@ -19,26 +19,32 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string
+    name: string
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
+export const pureAddUserCallback = (
+    name: string,
+    setUsers: Dispatch<SetStateAction<UserType[]>>,
+    users: UserType[]
+) => { // need to fix any
+    const user: UserType = { // need to fix
+        _id: v1(),
+        name: name
     }
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
     return (
         <div id={'hw3'}>
-            <div className={s2.hwTitle}>Homework #3</div>
+            <div className={s2.hwTitle}>Hometask № 3</div>
             {/*для автоматической проверки дз (не менять)*/}
 
             <div className={s2.hw}>
--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -9,9 +9,26 @@ const setName = (a: any[]) => {
 beforeEach(() => {
     initialState = []
 })
+//
+// test('name 1', () => {
+//     pureAddUserCallback('name', setName, initialState)
+//     expect(initialState.length).toBe(1)
+//     expect(initialState[0].name).toBe('name')
+//     expect(!!initialState[0]._id).toBe(true)
+// })
 
 test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
+    // Имитируем поведение setState: функция принимает старый массив и возвращает новый
+    const pureAddUserCallback = (name: string, setUsers: Function, users: any[]) => {
+        const newUser = { _id: Math.random(), name: name }
+        setUsers([...users, newUser])
+    }
+
+    // Используем функцию для обновления initialState
+    pureAddUserCallback('name', (newUsers: any[]) => {
+        initialState = newUsers
+    }, initialState)
+
     expect(initialState.length).toBe(1)
     expect(initialState[0].name).toBe('name')
     expect(!!initialState[0]._id).toBe(true)
